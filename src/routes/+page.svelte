8502076993880b1eb77c343702fb6ae5d6df2170
Shaima <shaimafuadi45@gmail.com>
2024-08-21 01:07:19 +0300

    added pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,10 @@
 <script>
-    import ThemeSwitch from "$lib/components/themeSwitch.svelte";
-import Button from "$lib/components/ui/button/button.svelte";
+
+    import { Button } from "$lib/components/ui/button";
 
 </script>
 
 
-<div class="flex justify-center items-center min-h-screen text-primary">
-   <div class="bg-secondary w-64 h-16 " >
-    
- 
-   </div>
-   <ThemeSwitch/>
-   hello world!
-   <Button>
-    click
-  </Button>
-</div>
+<Button href="/contact" variant="link" >
+    contact
+</Button>
