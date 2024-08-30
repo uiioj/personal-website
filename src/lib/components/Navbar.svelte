204c2161d07eca039c477e9f109e97080b26e1f1
Shaima <shaimafuadi45@gmail.com>
2024-08-30 23:24:56 +0300

    مشكلة الناف بار

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -6,19 +6,11 @@
     import Tele from "$lib/icons/tele.svelte";
     import Info from "$lib/icons/info.svelte";
     import Github from "$lib/icons/github.svelte";
+    import Flower from "$lib/icons/flower.svelte";
     import { page } from "$app/stores";
 
 
-    const links = [
-        {
-            text: 'Resume',
-            href: '/resume'
-        },
-        {
-            text:'About',
-            href: '/about'
-        }
-    ]
+   
 
 </script>
 
@@ -26,39 +18,68 @@
     class="flex justify-between w-[100dvw] h-fit bg-secondary text-secondary-foreground py-1 px-4"
 >
     <div class="flex items-center gap-2 text-xl">
-        <div class="bg-primary w-5 h-5 ms-3" />
-        <Button variant="link">
+
+        <div>
+            <Flower/>
+        </div>
+        <Button
+        class="{$page.url.pathname == '/shaima' &&
+                'text-accent'} flex md:hidden" 
+        href="/shaima" 
+        variant="link">
+         
             Shaima Fuad
 
         </Button>
     </div>
 
-    <div class="flex items-center p-1 ">
-    <div class="hidden sm:flex">
-        {#each links as link}
-        <Button
-        class="{$page.url.pathname == link.href &&
-                  link.text}"
-                  href={link.href}
-                  variant="link"
-                  >{link.text}
-  
-        </Button>
-  
-       {/each}
-    </div>
+    
+    <Button 
+        class=" {$page.url.pathname == '/resume' &&
+                'text-accent'}"
+        href="/resume" 
+        variant="link">
+            Resume
+        </Button> <!--السيرة بالخط -->
 
+        <Button 
+        class=" text-xl {$page.url.pathname == '/resume' &&
+                'text-accent'} flex md:hidden" 
+        href="/resume" 
+        variant="link">
+            <Resume/>
+        </Button> <!--السيرة بالرمز -->
+
+        <Button 
+        class=" {$page.url.pathname == '/about' &&
+                'text-accent'}" 
+        href="/about" 
+        variant="link">
+            About
+        </Button> <!--المعلومات بالخط -->
+
+        <Button 
+        class=" text-xl {$page.url.pathname == '/about' &&
+                'text-accent'} flex md:hidden" 
+        href="/about" 
+        variant="link">
+            <Info/>
+        </Button> <!--المعلومات بالرمز -->
     
 
+
+    
         <div
-         class="text-primary text-2xl text-primary/40 font-thin">
+         class="text-primary text-2xl text-primary/40 font-thin"> 
+         
+         <!--هذا الخط اللي بين الرسومات والكلمات-->
          |
         </div>
 
         <Button 
         class="text-2xl {$page.url.pathname == '/https://github.com/uiioj' &&
                 'text-accent'}" 
-        href="/" 
+        href="/https://github.com/uiioj" 
         variant="link">
             <Github />
         </Button>
@@ -66,13 +87,14 @@
         <Button 
         class="text-xl {$page.url.pathname == '/https://web.telegram.org/k/#@qur7illl' &&
                 'text-accent'}" 
-        href="/" 
+        href="/https://web.telegram.org/k/#@qur7illl" 
         variant="link">
             <Tele />
         </Button>
 
+    
+
         
-    </div>
 
 
 </nav>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,1 @@
-personal info.
+About
